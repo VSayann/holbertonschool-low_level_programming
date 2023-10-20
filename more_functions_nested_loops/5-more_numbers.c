486dac57859e2a19f787e40757beeddd24b45cfa
VSayann <sayann.valmond@gmail.com>
2023-10-20 06:29:22 -0700

    Task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 to 9 w/out 2 & 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+                printf("%d - %s\n", i+1, name);
+        }
+
+        return 0;
+}
