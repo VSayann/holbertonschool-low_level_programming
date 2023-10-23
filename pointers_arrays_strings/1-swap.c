1c9406e607c637d444a36bacdbb6cf76eeac193c
VSayann <sayann.valmond@gmail.com>
2023-10-23 05:35:55 -0700

    Task 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * swap_int - swap to int to 42
- * @a : number
- * @b : number
- * 
- * Return: Always 0.
+ * swap_int - swaps the values of two integers
+ * @a: pointer
+ * @b: pointer
  */
-
 void swap_int(int *a, int *b)
 {
 	int c;
