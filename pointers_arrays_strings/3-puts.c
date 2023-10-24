fda2ba48041c07cd6ee67ebe6312ba8c1a563fa8
VSayann <sayann.valmond@gmail.com>
2023-10-24 00:44:32 -0700

    Task 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * _puts : prints a string
+ * _puts - prints a string
  * @str : string
  */
 
 void _puts(char *str)
 {
-	while (*str != '\0') 
+	while (*str != '\0')
 	{
 		putchar(*str);
 		str++;
