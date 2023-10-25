af333b6afc56382d60c1394ee9538ca537d17810
VSayann <sayann.valmond@gmail.com>
2023-10-25 06:40:46 -0700

    Task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy copies the string pointed to
+ * @dest : string
+ * @src : string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *original_dest = dest;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return original_dest;
+}
