ec758cba649cc3cce21febf4b5b77f64d46f1b8d
VSayann <sayann.valmond@gmail.com>
2023-10-25 02:26:08 -0700

    Task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a : string
+ */
+
+void print_array(int *a, int n)
+{
+	int i
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
