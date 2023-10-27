c954e37d410e61866e1f4db2c944dcc549175cf7
VSayann <sayann.valmond@gmail.com>
2023-10-27 02:09:03 -0700

    Task 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include <string.h>
+
+/**
+ * reverse_array - reverses the content of an array
+ * @a: array
+ * @n: the size of the array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
+	}
+}
