f5b54aa5b27b9c0af5fa5b6dc6538fe068c6acd7
VSayann <sayann.valmond@gmail.com>
2023-10-28 04:47:04 -0700

    Task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s: the string to check
+ * @c: the char to locate
+ *
+ * Return: a pointer to the first occurence of c in s
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *retPointer = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			retPointer = s + i;
+			break;
+		}
+		i++;
+	}
+
+	if (c == '\0')
+		retPointer = s + i;
+
+	return (retPointer);
+}
