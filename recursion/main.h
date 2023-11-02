e7c27aa652a55b8b4429d9f2e9d775efd46b47df
VSayann <sayann.valmond@gmail.com>
2023-11-02 07:12:19 -0700

    main header

--- recursion/main.h
+++ recursion/main.h
@@ -1,8 +1,11 @@
-_puts_recursion(char *s);
+void _puts_recursion(char *s);
 
-_print_rev_recursion(char *s);
+void _print_rev_recursion(char *s);
 
 int _strlen_recursion(char *s);
 
 int factorial(int n);
 
+int _pow_recursion(int x, int y);
+
+int is_prime_number(int n);
