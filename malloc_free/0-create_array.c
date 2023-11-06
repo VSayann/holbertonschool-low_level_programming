1a6743194c36b432f05561e0f94b19995868f8fd
VSKen <141931123+VSayann@users.noreply.github.com>
2023-11-06 15:49:00 +0100

    Task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c) {
+    char *array = (char *)malloc(size * sizeof(char));
+
+    if (array == NULL) {
+        return NULL;
+    }
+
+    for (unsigned int i = 0; i < size; i++) {
+        array[i] = c;
+    }
+
+    return array;
+}
