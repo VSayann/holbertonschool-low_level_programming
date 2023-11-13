4d80c0b8be9dcd936face20b800682b6dd40538b
VSKen <141931123+VSayann@users.noreply.github.com>
2023-11-13 09:58:37 +0100

    Task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+void init_dog(struct dog *myDog, const char *name, float age, const char *owner)
+{
+    myDog->name = strdup(name);
+    myDog->age = age;
+    myDog->owner = strdup(owner);
+}
+
+void free_dog(struct dog *myDog) {
+    free(myDog->name);
+    free(myDog->owner);
+}
