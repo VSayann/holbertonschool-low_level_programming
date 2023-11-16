c192f0d8ab71d87160caf8d8a90d3170bf743881
VSKen <141931123+VSayann@users.noreply.github.com>
2023-11-16 11:22:13 +0100

    Task 4

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -21,5 +21,6 @@ void init_dog(struct dog *myDog, const char *name, float age, const char *owner)
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _atoi(char *s)
 
 #endif
