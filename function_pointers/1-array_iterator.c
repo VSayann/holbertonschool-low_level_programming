f925d41fec78db7acc5e3c261c27c4ffb355c4ce
VSKen <141931123+VSayann@users.noreply.github.com>
2023-11-16 13:49:27 +0100

    Task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,16 +2,15 @@
 
 /**
  * array_iterator - executes a function
- * 
  * @array: array
+ * @size: size of the array
  * @action: pointer to the function
- * 
+ *
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
     unsigned int i;
-
 	if (!array || !action)
 		return;
 
