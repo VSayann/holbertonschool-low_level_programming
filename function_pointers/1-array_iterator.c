1217fd711c07f063807d913e31bf51f320cb40b3
VSKen <141931123+VSayann@users.noreply.github.com>
2023-11-16 11:11:30 +0100

    Task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -10,5 +10,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    
+    unsigned int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
