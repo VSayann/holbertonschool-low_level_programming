6b7bf81d773d6ab2545f12836c5f8062a1507618
VSKen <141931123+VSayann@users.noreply.github.com>
2023-11-20 15:50:26 +0100

    Task 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,4 +4,5 @@
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
+print_all(const char * const format, ...)
 #endif
