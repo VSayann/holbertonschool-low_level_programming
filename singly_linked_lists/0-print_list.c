8524bb5f86a41c64c39b06cb4f6629531b105cf9
VSKen <141931123+VSayann@users.noreply.github.com>
2023-11-28 10:12:10 +0100

    Task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+size_t print_list(const list_t *h);
+
+	typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} 
+list_t;
---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,6 @@
+#ifndef LISTS_H
+#define LIST_T
+
+size_t print_list(const list_t *h);
+
+#endif
