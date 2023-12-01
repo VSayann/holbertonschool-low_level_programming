45f0d205b227df621a5a66b7274d036e9a688586
VSKen <141931123+VSayann@users.noreply.github.com>
2023-12-01 09:41:43 +0100

    Task 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+* add_node - adds a new node at the beginning of a list_t list
+* @head: original head node
+* @str: new node string
+* Return: new node
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode = malloc(sizeof(list_t));
+
+	if (newNode == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	if (str == NULL || head == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->str = strdup(str);
+
+	if (!(newNode->str))
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->len = strlen(str);
+	newNode->next = *head;
+
+	*head = newNode;
+
+	return (newNode);
+}
